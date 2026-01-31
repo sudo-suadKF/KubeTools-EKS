87b579ca95836e41ce13bc5ea315b914baf4ed3a
sudo-suadKF <suadfehric@outlook.com>
2026-01-31 23:33:53 +0100

    Modified: terraform fmt root files in bootstrap folder

--- infra/bootstrap/main.tf
+++ infra/bootstrap/main.tf
@@ -1,14 +1,14 @@
 module "ecr" {
-  source = "./modules/ecr"
-  ecr-repo-name = var.ecr-repo-name
+  source                  = "./modules/ecr"
+  ecr-repo-name           = var.ecr-repo-name
   ecr-repo-tag-mutability = var.ecr-repo-tag-mutability
 }
 
 module "s3" {
-  source = "./modules/s3"
-  s3-bucket-name = var.s3-bucket-name
-  s3-bucket-object-ownership = var.s3-bucket-object-ownership
-  s3-bucket-sse-algorithm = var.s3-bucket-sse-algorithm
-  s3-bucket-tag = var.s3-bucket-tag
+  source                      = "./modules/s3"
+  s3-bucket-name              = var.s3-bucket-name
+  s3-bucket-object-ownership  = var.s3-bucket-object-ownership
+  s3-bucket-sse-algorithm     = var.s3-bucket-sse-algorithm
+  s3-bucket-tag               = var.s3-bucket-tag
   s3-bucket-versioning-status = var.s3-bucket-versioning-status
 }
--- infra/bootstrap/variables.tf
+++ infra/bootstrap/variables.tf
@@ -1,41 +1,41 @@
 variable "ecr-repo-name" {
   description = "Contains ECR repo's name"
-  type = string
-  default = "kube2048-eks"
+  type        = string
+  default     = "kube2048-eks"
 }
 
 variable "ecr-repo-tag-mutability" {
   description = "Contains ECR repo's tag mutability value"
-  type = string
-  default = "MUTABLE"
+  type        = string
+  default     = "MUTABLE"
 }
 
 variable "s3-bucket-name" {
   description = "Contains s3 bucket's name"
-  type = string
-  default = "eks-bootstrap-bucket-for-terraform-state"
+  type        = string
+  default     = "eks-bootstrap-bucket-for-terraform-state"
 }
 
 variable "s3-bucket-tag" {
   description = "Contains s3 bucket's tag"
-  type = string
-  default = "EKS bootstrap bucket"
+  type        = string
+  default     = "EKS bootstrap bucket"
 }
 
 variable "s3-bucket-object-ownership" {
   description = "Contains object ownership's value"
-  type = string
-  default = "BucketOwnerEnforced"
+  type        = string
+  default     = "BucketOwnerEnforced"
 }
 
 variable "s3-bucket-versioning-status" {
   description = "Contains versioning status value"
-  type = string
-  default = "Enabled"
+  type        = string
+  default     = "Enabled"
 }
 
 variable "s3-bucket-sse-algorithm" {
   description = "Contains sse algorithm's value"
-  type = string
-  default = "AES256"
+  type        = string
+  default     = "AES256"
 }
